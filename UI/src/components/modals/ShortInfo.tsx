661947597d836bda86bc8a82410ae2a2e29bd4d3
Myroslav13 <myroslavshchuruk@gmail.com>
2026-01-25 12:35:50 +0200

    Modals completed

--- UI/src/components/modals/ShortInfo.tsx
+++ UI/src/components/modals/ShortInfo.tsx
@@ -1,5 +1,5 @@
-import { HiOutlineXCircle } from "react-icons/hi2";
-import { HiOutlineCheckCircle } from "react-icons/hi2";
+import { BsExclamationTriangle } from "react-icons/bs";
+import { BsCheckCircle } from "react-icons/bs";
 
 interface ShortInfoProps {
    title: string,
@@ -10,12 +10,15 @@ interface ShortInfoProps {
 
 function ShortInfo({title, text, isOk, setShowModal} : ShortInfoProps) {
    return (
-      <div className="absolute inset-inline-0 bg-white p-5 rounded-xl z-90">
+      <div className="absolute inset-inline-0 bg-white p-5 rounded-xl z-90 w-[300px]">
          <div className="w-[95%] flex flex-col justify-center items-center">
-            {isOk ? <HiOutlineXCircle/> : <HiOutlineCheckCircle/>}
-            <h1 className="text-[#1E2A38] text-lg font-bold">{title}</h1>
+            {isOk ? <BsCheckCircle className="modal-img text-green-500"/> : <BsExclamationTriangle className="modal-img text-yellow-500" />}
+            <h1 className="text-[#1E2A38] text-lg font-bold mt-2">{title}</h1>
             <p className="text-center mt-2">{text}</p>
-            {isOk ? <button className="w-[100%] bg-[#00C9A7] text-white h-10 rounded-2xl font-bold mt-5 cursor-pointer" onClick={() => setShowModal(false)}>Great</button> : <button onClick={() => setShowModal(false)} className="w-[100%] border-3 border-gray-500 text-gray-500 h-10 rounded-2xl font-bold mt-5 cursor-pointer">Close</button>}
+            {isOk ? 
+            <button className="bg-[#00C9A7] text-white modal-btn" onClick={() => setShowModal(false)}>Great</button> 
+            : 
+            <button className="border-3 border-gray-500 text-gray-500 modal-btn" onClick={() => setShowModal(false)}>Close</button>}
          </div>
       </div>
    );
