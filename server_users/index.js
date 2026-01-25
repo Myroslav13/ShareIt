70be8b9ae63fc22f6e1b8833df234c63f49c1380
Myroslav13 <myroslavshchuruk@gmail.com>
2026-01-25 14:10:50 +0200

    Started to work with authentication

--- server_users/index.js
+++ server_users/index.js
@@ -1,6 +1,11 @@
 import express from "express";
 import pg from "pg";
 import env from "dotenv";
+import bcrypt from "bcrypt";
+import passport from "passport";
+import session from "express-session";
+import { Strategy as LocalStrategy } from "passport-local";
+import GoogleStrategy from "passport-google-oauth2";
 
 env.config();
  
@@ -16,9 +21,42 @@ db.connect();
 
 const port = 3000;
 const app = express();
+const saltRounds = 10;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(session({
+   secret: process.env.COOKIES_SECRET,
+   resave: false,
+   saveUninitialized: true,
+   cookie: {
+      maxAge: 1000 * 60 * 60,
+   }
+}));
+
+passport.use("local", new LocalStrategy(
+   {usernameField: "email", passwordField: "password"},
+   async (username, password, cb) => {
+      
+   }
+));
+
+app.post("/login", (req, res) => {
+   passport.authenticate("local", (error, user) => {
+
+   });
+});
+
+passport.serializeUser((user, done) => {
+   done(null, user.id);
+});
+
+passport.deserializeUser(async (userId, done) => {
+   const response = await db.query("SELECT * FROM users WHERE id = $1", [userId]);
+   const result = response.rows[0];
+   done(null, result);
+});
 
 app.listen(port, () => {
    console.log(`Successfully listening to ${port}`);
