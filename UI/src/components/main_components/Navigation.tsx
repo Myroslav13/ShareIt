9bb270f0f8f11c85300c2e9bb7072bcb7eaadd1d
Myroslav13 <myroslavshchuruk@gmail.com>
2026-01-26 21:42:44 +0200

    UI navigation completed

--- UI/src/components/main_components/Navigation.tsx
+++ UI/src/components/main_components/Navigation.tsx
@@ -17,19 +17,19 @@ function Navigation() {
 
    return (
       <>
-         <div className="border-b flex items-center justify-between p-5">
-            <h1 className="text-2xl font-bold">ShareIt</h1>
+         <div className="border-b border-gray-400 flex items-center justify-between p-5">
+            <h1 className="text-3xl font-bold">Share<span className="text-[#00C9A7]">It</span></h1>
 
             <div className="flex items-center gap-4">
-               <button className="">Rent out</button>
-               <BsPersonCircle />
+               <button className="bg-[#00C9A7] text-white rounded-2xl font-bold shadow px-4 py-2 cursor-pointer hover:bg-[#00A88D]">Rent out</button>
+               <BsPersonCircle size={30} className="cursor-pointer" />
             </div>
          </div>
 
          <div className="flex w-[100%] items-stretch items-center justify-center mt-4">
-            <div className="flex w-[60%] p-4 items-center justify-between border-1 rounded-2xl divide-x divide-black">
+            <div className="flex w-[60%] p-4 items-center justify-between border-1 border-gray-400 rounded-2xl divide-x divide-gray-400">
                <div className="flex-1 flex items-center gap-4 px-4">
-                  <BsSearch />
+                  <BsSearch size={25} />
                   <input
                      value={itemName}
                      onChange={(e) => setItemName(e.target.value)}
@@ -38,7 +38,7 @@ function Navigation() {
                   />
                </div>
                <div className="flex-1 flex items-center gap-4 px-4">
-                  <BsCalendar4Event />
+                  <BsCalendar4Event size={25} />
                   <input
                      type="date"
                      value={date}
@@ -48,7 +48,7 @@ function Navigation() {
                </div>
 
                <div className="flex-1 flex items-center gap-4 px-4">
-                  <BsFillGeoAltFill />
+                  <BsFillGeoAltFill size={25} />
                   <input
                      list="places"
                      value={location}
@@ -65,8 +65,8 @@ function Navigation() {
                   </datalist>
                </div>      
             </div>
-            <button className="ml-2 bg-[#00C9A7] text-white rounded-full flex items-center justify-center w-[75px] cursor-pointer">
-               <BsSearch />
+            <button className="ml-2 bg-[#00C9A7] text-white rounded-full flex items-center justify-center w-[75px] cursor-pointer hover:bg-[#00A88D]">
+               <BsSearch size={20} />
             </button>
          </div>
       </>
